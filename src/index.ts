40fea13ca849c441c895a855897dbefa162ba07b
takeyu1013 <yuto1013@gmail.com>
2022-01-08 15:26:15 +0900

    ➕ Add CORS

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
 import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+import cors from "fastify-cors";
 
 const prisma = new PrismaClient();
-const app = fastify();
+const app = fastify().register(cors);
 
-app.get("/users", async (req, res) => {
+app.get("/users", async (_, res) => {
   const users = await prisma.user.findMany();
   res.send(users);
 });
 
-app.listen(3000, (err) => {
+app.get("/", async (_, res) => {
+  res.send("Hello World");
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, "0.0.0.0", (err) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
   console.log(`
-  🚀 Server ready at: http://localhost:3000
+  🚀 Server ready at: http://localhost:${port}
   ⭐️ See sample requests: http://pris.ly/e/ts/rest-fastify#3-using-the-rest-api`);
 });
