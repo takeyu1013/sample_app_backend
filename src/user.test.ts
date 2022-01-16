938390997fc4e322b590fb1bae3cb0b03a3c26e2
takeyu1013 <yuto1013@gmail.com>
2022-01-16 23:12:09 +0900

    ✅ Refactor tests

--- src/user.test.ts
+++ src/user.test.ts
@@ -1,114 +1,53 @@
-import { PrismaClient, User } from "@prisma/client";
-
-import type { Body } from "./index";
-import { app } from "./index";
+import { PrismaClient } from "@prisma/client";
+import type { Body } from "./user";
+import { createUser } from "./user";
 
 describe("user tests", () => {
   const prisma = new PrismaClient();
-  beforeEach(() => {
-    return prisma.user.deleteMany();
+  beforeAll(async () => {
+    await prisma.user.deleteMany();
   });
-
-  const defaultUser: Body = {
+  const user: Body = {
     name: "Example User",
     email: "user@example.com",
+    password: "foobar",
+    passwordConfirmation: "foobar",
   };
 
   test("should be vaild", async () => {
-    const response = await app.inject({
-      method: "POST",
-      url: "/users",
-      payload: defaultUser,
-    });
-    expect(response.statusCode).toBe(201);
+    expect(await createUser(user)).toBeTruthy();
   });
 
   test("name should be present", async () => {
-    const user = { ...defaultUser, name: "" };
-    const response = await app.inject({
-      method: "POST",
-      url: "/users",
-      payload: user,
-    });
-    expect(response.statusCode).toBe(400);
+    expect(() => createUser({ ...user, name: "" })).rejects.toThrowError();
   });
 
   test("email should be present", async () => {
-    const user = { ...defaultUser, email: "     " };
-    const response = await app.inject({
-      method: "POST",
-      url: "/users",
-      payload: user,
-    });
-    expect(response.statusCode).toBe(400);
+    expect(() =>
+      createUser({ ...user, email: "     " })
+    ).rejects.toThrowError();
   });
 
   test("name should not be too long", async () => {
-    const user = { ...defaultUser, name: "a".repeat(51) };
-    const response = await app.inject({
-      method: "POST",
-      url: "/users",
-      payload: user,
-    });
-    expect(response.statusCode).toBe(400);
+    expect(() =>
+      createUser({ ...user, name: "a".repeat(51) })
+    ).rejects.toThrowError();
   });
 
   test("email should not be too long", async () => {
-    const user = { ...defaultUser, email: "a".repeat(244) + "@example.com" };
-    const response = await app.inject({
-      method: "POST",
-      url: "/users",
-      payload: user,
-    });
-    expect(response.statusCode).toBe(400);
-  });
-
-  test("email validation should reject invalid addresses", async () => {
-    const invaildAddresses = [
-      "user@example,com",
-      "user_at_foo.org",
-      "user.name@example.foo@bar_baz.com",
-      "foo@bar+baz.com",
-    ];
-    invaildAddresses.map(async (email) => {
-      const user = { ...defaultUser, email };
-      const response = await app.inject({
-        method: "POST",
-        url: "/users",
-        payload: user,
-      });
-      expect(response.statusCode).toBe(400);
-    });
+    expect(() =>
+      createUser({ ...user, email: "a".repeat(244) + "@example.com" })
+    ).rejects.toThrowError();
   });
 
   test("email addresses should be unique", async () => {
-    await app.inject({
-      method: "POST",
-      url: "/users",
-      payload: defaultUser,
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        passwordDigest: user.password,
+      },
     });
-    const user = { ...defaultUser, email: defaultUser.email.toUpperCase() };
-    const response = await app.inject({
-      method: "POST",
-      url: "/users",
-      payload: user,
-    });
-    expect(response.statusCode).toBe(409);
-  });
-
-  test("email addresses should be saved as lower-case", async () => {
-    const mixedCaseEmail = "Foo@ExAMPle.CoM";
-    const user = { ...defaultUser, email: mixedCaseEmail };
-    const response = await app.inject({
-      method: "POST",
-      url: "/users",
-      payload: user,
-    });
-    expect(response.json<User>().email).toBe(mixedCaseEmail.toLowerCase());
-  });
-
-  afterAll((done) => {
-    app.close();
-    done();
+    expect(() => createUser(user)).rejects.toThrowError();
   });
 });
